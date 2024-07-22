c3097d6bdc929e2baba1cef442ab3ce4f95314b2
PANdas <krsy0411@naver.com>
2024-07-23 03:52:46 +0900

    마이페이지 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import App from './App';
 import Login from './Login/Login';
 import CollectionBook from './CollectionBook/Collection';
+import MyPage from './MyPage/MyPage';
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
         <Route path="/" element={<App />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/collection" element={<CollectionBook />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
       </Routes>
     </>
   );
