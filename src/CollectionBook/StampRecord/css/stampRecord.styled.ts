d767ce5f829204a033fe740e5c3db736ecea340a
PANdas <krsy0411@naver.com>
2024-07-23 03:52:15 +0900

    컬렉션북 - 스탬프 기록 컴포넌트 구현

--- src/CollectionBook/StampRecord/css/stampRecord.styled.ts
+++ src/CollectionBook/StampRecord/css/stampRecord.styled.ts
@@ -4,6 +4,7 @@ export const StampRecordCotainer = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: start;
+  padding-top: 55px;
 `;
