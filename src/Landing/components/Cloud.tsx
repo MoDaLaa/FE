c4f8207427ecd5540012fb8d75736116cd9b377e
PANdas <krsy0411@naver.com>
2024-08-11 20:04:53 +0900

    랜딩 페이지 구현 완료

---
+++ src/Landing/components/Cloud.tsx
@@ -0,0 +1,14 @@
+import * as Styled from '../css/cloud.styled';
+
+export interface CloudPropsType {
+  $cloudFileName: string;
+  $size: string;
+  $posX: number;
+  $posY: number;
+}
+
+export default function Cloud({ $cloudFileName, $size, $posX, $posY }: CloudPropsType) {
+  return (
+    <Styled.Img src={`/cloud/${$cloudFileName}.png`} $size={$size} $posX={$posX} $posY={$posY} />
+  );
+}
