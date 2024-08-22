e80a12da8782fce9dbf8d47a2ce7c7cb91bdd87a
PANdas <krsy0411@naver.com>
2024-08-22 21:53:19 +0900

    KAKAO Oauth 구현 & Router path 재설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,12 +8,14 @@ import CollectionBook from './CollectionBook/Collection';
 import MyPage from './MyPage/MyPage';
 import Landing from './Landing/Landing';
 import Modal from './shared/Modal/Modal';
+import Redirection from './shared/Redirection/Redirection';
 
 export default function Router() {
   return (
     <>
       <Routes>
-        m<Route path="/" element={<Landing />}></Route>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/landing" element={<Landing />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/shopping" element={<Shopping />}></Route>
         {/* Shoppingcontent = shopping/1와 같이 설정해야함 */}
@@ -21,7 +23,8 @@ export default function Router() {
         <Route path="/collection" element={<CollectionBook />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/modal" element={<Modal />}></Route>
-        <Route path="/main" element={<Main />}></Route>
+        {/* 소셜로그인 하는 경우의 리디렉션 페이지 */}
+        <Route path="/redirect" element={<Redirection />}></Route>
       </Routes>
     </>
   );
