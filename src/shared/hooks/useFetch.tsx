60a6dc3b517f8d2ac243d712d45121d4a2a0e2cc
PANdas <krsy0411@naver.com>
2024-08-22 18:44:04 +0900

    현민이 코드 충돌나는 부분 해결(런타임X)

--- src/shared/hooks/useFetch.tsx
+++ src/shared/hooks/useFetch.tsx
@@ -1,50 +1,50 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-type ConfigTypes = {
-  mode?: string;
-  cache?: string;
-  credentials?: string;
-  headers?: object;
-  redirect?: string;
-  referrerPolicy?: string;
-};
-interface useFetchParamsType {
-  url: string;
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
-  configOptions?: ConfigTypes;
-  body?: BodyInit;
-}
-interface useFetchReturnType {
-  data: string | null;
-  errorMessage: string | null;
-}
+// type ConfigTypes = {
+//   mode?: string;
+//   cache?: string;
+//   credentials?: string;
+//   headers?: object;
+//   redirect?: string;
+//   referrerPolicy?: string;
+// };
+// interface useFetchParamsType {
+//   url: string;
+//   method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+//   configOptions?: ConfigTypes;
+//   body?: BodyInit;
+// }
+// interface useFetchReturnType {
+//   data: string | null;
+//   errorMessage: string | null;
+// }
 
-export default async function useFetch({
-  url,
-  method,
-  configOptions = {},
-  body,
-}: useFetchParamsType): Promise<useFetchReturnType> {
-  const [data, setData] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
+// export default async function useFetch({
+//   url,
+//   method,
+//   configOptions = {},
+//   body,
+// }: useFetchParamsType): Promise<useFetchReturnType> {
+//   const [data, setData] = useState(null);
+//   const [errorMessage, setErrorMessage] = useState(null);
 
-  // 초기 한번만 실행됩니다
-  useEffect(() => {
-    // method, config는 필수 요청 요소니까 초기화시킴과 동시에 할당
-    let initOptions: Pick<useFetchParamsType, 'method' | 'configOptions' | 'body'> = {
-      method,
-      ...configOptions,
-    };
-    // 만약 바디 데이터가 존재하면(undefined X), JSON으로 변환시켜서 옵션에 추가
-    if (typeof body !== 'undefined') {
-      initOptions = { ...initOptions, body: JSON.stringify(body) };
-    }
+//   // 초기 한번만 실행됩니다
+//   useEffect(() => {
+//     // method, config는 필수 요청 요소니까 초기화시킴과 동시에 할당
+//     let initOptions: Pick<useFetchParamsType, 'method' | 'configOptions' | 'body'> = {
+//       method,
+//       ...configOptions,
+//     };
+//     // 만약 바디 데이터가 존재하면(undefined X), JSON으로 변환시켜서 옵션에 추가
+//     if (typeof body !== 'undefined') {
+//       initOptions = { ...initOptions, body: JSON.stringify(body) };
+//     }
 
-    const reponse = await fetch(url, initOptions);
-  }, []);
+//     fetch(url, initOptions);
+//   }, []);
 
-  return {
-    data,
-    errorMessage,
-  };
-}
+//   return {
+//     data,
+//     errorMessage,
+//   };
+// }
