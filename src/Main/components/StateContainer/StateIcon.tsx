7998db70728d29706295adb3021840c98b89464f
minzzn <188min.kk@gmail.com>
2024-08-24 00:46:22 +0900

    fix: 컴파일 시 stateicon 타입 미지정 오류 관련  수정

--- src/Main/components/StateContainer/StateIcon.tsx
+++ src/Main/components/StateContainer/StateIcon.tsx
@@ -2,12 +2,17 @@ import * as Styled from '../../css/icon.button.styled';
 import sunny from '../../../../public/png/sun.icon.png';
 import stamp from '../../../../public/png/stamp.icon.png';
 
-const iconMap = {
+// StateIconProps 타입 정의
+type StateIconProps = {
+    state: 'sunny' | 'stamp'; // 가능한 state 값 정의
+};
+
+const iconMap: { [key in StateIconProps['state']]: string } = {
     sunny: sunny,
     stamp: stamp,
 };
 
-export default function StateIcon({ state }) {
+export default function StateIcon({ state }: StateIconProps) {
     const iconSrc = iconMap[state] || null;
 
     return (
