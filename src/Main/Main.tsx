fe449b41f51dd77cd8849c1e92812f5de4a43067
minzzn <188min.kk@gmail.com>
2024-08-27 22:23:19 +0900

    fix: 전체 map svg 관련 메인페이지 수정 중

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -3,32 +3,49 @@ import TopAppBar from './components/TopAppBar';
 import StateBox from './components/StateContainer/StateContainer';
 import DateandAreaInfo from './components/DateAreaInfo';
 import Map from './components/Map/Map';
-import BottomNavigationBar from './components/BottomNavigationBar';
 import * as Styled from "./css/main.styled";
 import MainCloudBackground from './components/MainCloudBackground';
+// import RegionBubble from './components/RegionBubble';
+
+// interface Region {
+//     region: string;
+//     top: string;
+//     left: string;
+// }
 
 export default function Main() {
-    {/* css로 maincontainer에 따로 배경색을 입힐 수 있으나 스크롤시 이벤트를 감지하여
-     따로 배경색을 추가하여 설정해줘야 했기 때문에, 아래와 같이 useEffect를 사용하여 컴포넌트 렌더링 시 body 배경색 변경 */}
     useEffect(() => {
-        // 컴포넌트가 마운트될 때 body 배경색 설정
         document.body.style.backgroundColor = '#D6EFAE';
-    
-        // 컴포넌트가 언마운트될 때 배경색 초기화
         return () => {
-          document.body.style.backgroundColor = '';
+            document.body.style.backgroundColor = '';
         };
     }, []);
 
+    // const regions: Region[] = [
+    //     { region: '경주시내권', top: "335", left: "116" },
+    //     { region: '불국사권', top: "389", left: "190" },
+    //     { region: '북부권', top: "249", left: "126" },
+    //     { region: '서부권', top: "376", left: "44" },
+    //     { region: '동해권', top: "402", left: "273" },
+    //     { region: '보문권', top: "335", left: "216" },
+    //     { region: '남산권', top: "416", left: "133" },
+    // ];
 
     return (
         <Styled.MainContainer>
             <TopAppBar />
             <DateandAreaInfo />
             <StateBox />
+            {/* {regions.map((region) => (
+                <RegionBubble 
+                    key={region.region} 
+                    region={region.region} 
+                    style={{position:'absolute', top: `${region.top}px`, left: `${region.left}px`}}
+                    isSvg={true}
+                />
+            ))} */}
             <Map />
             <MainCloudBackground />
-            <BottomNavigationBar />  
         </Styled.MainContainer>
     );
 }
