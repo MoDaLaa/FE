ac06dbae6df6929ddeedcb6b8e97a34da481d66d
PANdas <krsy0411@naver.com>
2024-09-02 21:38:08 +0900

    modal 구현중

--- src/shared/Modal/Modal.tsx
+++ src/shared/Modal/Modal.tsx
@@ -1,7 +1,33 @@
-export default function Modal() {
-  return (
-    <div className="modal-container">
-      <p>모달 화면입니다</p>
-    </div>
-  );
-}
+// import { ReactNode, useState } from 'react';
+// import ReactModal from 'react-modal';
+// import { ModalContainer } from './style/modal.styled';
+
+// const modalCustomStyle = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//   },
+// };
+
+// // https://github.com/reactjs/react-modal
+// // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+// ReactModal.setAppElement('#root');
+
+// interface ModalPropsType {
+//   children: ReactNode;
+// }
+// export default function Modal({ children }: ModalPropsType) {
+//   const [modalIsOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <>
+//       <ReactModal isOpen={modalIsOpen} style={modalCustomStyle}>
+//         {children}
+//       </ReactModal>
+//     </>
+//   );
+// }
---
+++ src/shared/Modal/style/modal.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  width: 340px;
+  height: 510px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
